c317bbf57cdd0ceeab0e732d06f643a1125185f8
Henk van der Kuur <info@vanderkuur.com>
2020-09-24 08:34:24 +0200

    Versie 1.0

---
+++ src/Tafels.java
@@ -0,0 +1,35 @@
+public class Tafels {
+    public static void main(String[] args) {
+        var a = 93;
+        var b = a * 2;
+        var c = a * 3;
+        var d = a * 4;
+        var e = a * 5;
+        var f = a * 6;
+        var g = a * 7;
+        var h = a * 8;
+        var i = a * 9;
+        var j = a * 10;
+        System.out.print("1 x " + a);
+        System.out.println(" = " + a);
+        System.out.print("2 x " +a);
+        System.out.println(" = " + b);
+        System.out.print("3 x " +a);
+        System.out.println(" = " + c);
+        System.out.print("4 x " +a);
+        System.out.println(" = " + d);
+        System.out.print("5 x " +a);
+        System.out.println(" = " + e);
+        System.out.print("6 x " +a);
+        System.out.println(" = " + f);
+        System.out.print("7 x " +a);
+        System.out.println(" = " + g);
+        System.out.print("8 x " +a);
+        System.out.println(" = " + h);
+        System.out.print("9 x " +a);
+        System.out.println(" = " + i);
+        System.out.print("10 x " +a);
+        System.out.println(" = " + j);
+
+    }
+}
